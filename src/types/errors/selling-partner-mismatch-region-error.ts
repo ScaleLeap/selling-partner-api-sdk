f0c2fe344a09f934a2ab5145d444bee6097af8de
Toan Nguyen <nguyentoandev@gmail.com>
2021-04-01 17:23:01 +0700

    chore: update SellingPartnerMismatchRegionError error message format

--- src/types/errors/selling-partner-mismatch-region-error.ts
+++ src/types/errors/selling-partner-mismatch-region-error.ts
@@ -2,8 +2,7 @@ import { ExtendableError } from 'ts-error'
 
 export class SellingPartnerMismatchRegionError extends ExtendableError {
   public constructor(public defaultRegion: string, public region: string) {
-    super(`
-      There is a mismatch between region parameter and region in default base path!
+    super(`There is a mismatch between region parameter and region in default base path!
       Default Region: ${defaultRegion}
       Region: ${region}
     `)
