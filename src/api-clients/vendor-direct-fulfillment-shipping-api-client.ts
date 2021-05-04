67e0d4051fb28ded350f1da0fe4dc3e92c5cd6ca
nguyentoanit <nguyentoanit@users.noreply.github.com>
2021-05-04 04:28:14 +0000

    chore: update api models

---
+++ src/api-clients/vendor-direct-fulfillment-shipping-api-client.ts
@@ -0,0 +1,17 @@
+import {
+  Configuration,
+  CustomerInvoicesApi,
+} from '../api-models/vendor-direct-fulfillment-shipping-api-model'
+import { ApiClientHelpers } from '../helpers'
+import { DEFAULT_API_BASE_PATH } from '../types'
+import { APIConfigurationParameters } from '../types/api-clients/api-configuration-parameters'
+
+export class VendorDirectFulfillmentShippingApiClient extends CustomerInvoicesApi {
+  constructor(parameters: APIConfigurationParameters) {
+    const axios = ApiClientHelpers.getAxiosInstance(parameters)
+
+    const configuration = new Configuration(parameters)
+
+    super(configuration, DEFAULT_API_BASE_PATH, axios)
+  }
+}
