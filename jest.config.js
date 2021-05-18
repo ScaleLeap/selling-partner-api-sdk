19ed8b9354d42a4b9808170e3b0f67baa255cb1f
Toan Nguyen <nguyentoandev@gmail.com>
2021-05-18 15:19:41 +0700

    feat: attempt to get rate limit value

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   coveragePathIgnorePatterns: ['<rootDir>/src/api-models/'],
+  setupFilesAfterEnv: ['@scaleleap/jest-polly'],
 }
