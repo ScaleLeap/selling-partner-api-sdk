f16529e6c83406885057aa58722bf0853beb6f1d
nguyentoanit <nguyentoanit@users.noreply.github.com>
2021-06-23 09:42:48 +0000

    chore: update api models

---
+++ src/api-clients/catalog-items-api-client-v20201201.ts
@@ -0,0 +1,14 @@
+import { CatalogApi, Configuration } from '../api-models/catalog-items-api-model-v20201201'
+import { ApiClientHelpers } from '../helpers'
+import { DEFAULT_API_BASE_PATH } from '../types'
+import { APIConfigurationParameters } from '../types/api-clients/api-configuration-parameters'
+
+export class CatalogItemsApiClientV20201201 extends CatalogApi {
+  constructor(parameters: APIConfigurationParameters) {
+    const axios = ApiClientHelpers.getAxiosInstance(parameters)
+
+    const configuration = new Configuration(parameters)
+
+    super(configuration, DEFAULT_API_BASE_PATH, axios)
+  }
+}
---
+++ src/api-clients/reports-api-client-v20210630.ts
@@ -0,0 +1,14 @@
+import { Configuration, ReportsApi } from '../api-models/reports-api-model-v20210630'
+import { ApiClientHelpers } from '../helpers'
+import { DEFAULT_API_BASE_PATH } from '../types'
+import { APIConfigurationParameters } from '../types/api-clients/api-configuration-parameters'
+
+export class ReportsApiClientV20210630 extends ReportsApi {
+  constructor(parameters: APIConfigurationParameters) {
+    const axios = ApiClientHelpers.getAxiosInstance(parameters)
+
+    const configuration = new Configuration(parameters)
+
+    super(configuration, DEFAULT_API_BASE_PATH, axios)
+  }
+}
