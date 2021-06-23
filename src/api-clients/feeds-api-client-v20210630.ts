c32a66310c1168d0ebfd95f8185388f3490c7df1
nguyentoanit <nguyentoanit@users.noreply.github.com>
2021-06-23 03:04:49 +0000

    chore: update api models

---
+++ src/api-clients/feeds-api-client-v20210630.ts
@@ -0,0 +1,14 @@
+import { Configuration, FeedsApi } from '../api-models/feeds-api-model-v20210630'
+import { ApiClientHelpers } from '../helpers'
+import { DEFAULT_API_BASE_PATH } from '../types'
+import { APIConfigurationParameters } from '../types/api-clients/api-configuration-parameters'
+
+export class FeedsApiClientV20210630 extends FeedsApi {
+  constructor(parameters: APIConfigurationParameters) {
+    const axios = ApiClientHelpers.getAxiosInstance(parameters)
+
+    const configuration = new Configuration(parameters)
+
+    super(configuration, DEFAULT_API_BASE_PATH, axios)
+  }
+}
