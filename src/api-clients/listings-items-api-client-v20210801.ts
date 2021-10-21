f24b14ecc8f49aa9e67a2aefdf06903a2c49177a
nguyentoanit <nguyentoanit@users.noreply.github.com>
2021-10-21 01:30:15 +0000

    chore: update api models

---
+++ src/api-clients/listings-items-api-client-v20210801.ts
@@ -0,0 +1,14 @@
+import { Configuration, ListingsApi } from '../api-models/listings-items-api-model-v20210801'
+import { ApiClientHelpers } from '../helpers'
+import { DEFAULT_API_BASE_PATH } from '../types'
+import { APIConfigurationParameters } from '../types/api-clients/api-configuration-parameters'
+
+export class ListingsItemsApiClientV20210801 extends ListingsApi {
+  constructor(parameters: APIConfigurationParameters) {
+    const axios = ApiClientHelpers.getAxiosInstance(parameters)
+
+    const configuration = new Configuration(parameters)
+
+    super(configuration, DEFAULT_API_BASE_PATH, axios)
+  }
+}
---
+++ src/api-clients/listings-restrictions-api-client.ts
@@ -0,0 +1,14 @@
+import { Configuration, ListingsApi } from '../api-models/listings-restrictions-api-model'
+import { ApiClientHelpers } from '../helpers'
+import { DEFAULT_API_BASE_PATH } from '../types'
+import { APIConfigurationParameters } from '../types/api-clients/api-configuration-parameters'
+
+export class ListingsRestrictionsApiClient extends ListingsApi {
+  constructor(parameters: APIConfigurationParameters) {
+    const axios = ApiClientHelpers.getAxiosInstance(parameters)
+
+    const configuration = new Configuration(parameters)
+
+    super(configuration, DEFAULT_API_BASE_PATH, axios)
+  }
+}
