c9e12988b1603fbc697d372c121f4d0a7927cf88
Toan Nguyen <nguyentoandev@gmail.com>
2022-05-18 19:24:35 +0700

    fix: handle all unknown errors (#454)

--- src/helpers/api-error-factory.ts
+++ src/helpers/api-error-factory.ts
@@ -16,47 +16,52 @@ import {
   SellingPartnerUnsupportedMediaTypeError,
 } from '../types'
 
-export function apiErrorFactory<T extends ModelErrorContainer>(
-  error: AxiosError<T>,
-): ExtendableError {
-  const { response } = error
+function isAPIModelError(data: unknown): data is ModelErrorContainer {
+  return (data as ModelErrorContainer).errors?.every(
+    (element) => 'code' in element && 'message' in element,
+  )
+}
+
+export function apiErrorFactory(error: AxiosError): ExtendableError {
+  const { response, code, message } = error
 
   if (response) {
-    const { headers, data } = response
+    const { headers, data, status } = response
 
-    const modelError = data?.errors?.shift()
+    if (isAPIModelError(data)) {
+      const modelError = data.errors.shift()
+      if (modelError === undefined) {
+        return new SellingPartnerUnknownError(
+          {
+            code: code || 'UnknownError',
+            message,
+          },
+          headers,
+        )
+      }
 
-    if (modelError === undefined) {
-      return new SellingPartnerUnknownError(
-        {
-          code: 'UnknownError',
-          message: 'Selling Partner API unknown error',
-        },
-        headers,
-      )
+      switch (status) {
+        case StatusCodes.BAD_REQUEST:
+          return new SellingPartnerBadRequestError(modelError, headers)
+        case StatusCodes.FORBIDDEN:
+          return new SellingPartnerForbiddenError(modelError, headers)
+        case StatusCodes.NOT_FOUND:
+          return new SellingPartnerNotFoundError(modelError, headers)
+        case StatusCodes.REQUEST_TOO_LONG:
+          return new SellingPartnerRequestTooLongError(modelError, headers)
+        case StatusCodes.UNSUPPORTED_MEDIA_TYPE:
+          return new SellingPartnerUnsupportedMediaTypeError(modelError, headers)
+        case StatusCodes.TOO_MANY_REQUESTS:
+          return new SellingPartnerTooManyRequestsError(modelError, headers)
+        case StatusCodes.INTERNAL_SERVER_ERROR:
+          return new SellingPartnerInternalServerError(modelError, headers)
+        case StatusCodes.SERVICE_UNAVAILABLE:
+          return new SellingPartnerServiceUnavailableError(modelError, headers)
+        default:
+          return new SellingPartnerGenericError(modelError, headers)
+      }
     }
-
-    switch (response.status) {
-      case StatusCodes.BAD_REQUEST:
-        return new SellingPartnerBadRequestError(modelError, headers)
-      case StatusCodes.FORBIDDEN:
-        return new SellingPartnerForbiddenError(modelError, headers)
-      case StatusCodes.NOT_FOUND:
-        return new SellingPartnerNotFoundError(modelError, headers)
-      case StatusCodes.REQUEST_TOO_LONG:
-        return new SellingPartnerRequestTooLongError(modelError, headers)
-      case StatusCodes.UNSUPPORTED_MEDIA_TYPE:
-        return new SellingPartnerUnsupportedMediaTypeError(modelError, headers)
-      case StatusCodes.TOO_MANY_REQUESTS:
-        return new SellingPartnerTooManyRequestsError(modelError, headers)
-      case StatusCodes.INTERNAL_SERVER_ERROR:
-        return new SellingPartnerInternalServerError(modelError, headers)
-      case StatusCodes.SERVICE_UNAVAILABLE:
-        return new SellingPartnerServiceUnavailableError(modelError, headers)
-      default:
-        return new SellingPartnerGenericError(modelError, headers)
-    }
-  } else {
-    return error
   }
+
+  return error
 }
