cbba0406dc5cae4862aaf4c54d744b837b479ec3
nguyentoanit <nguyentoanit@users.noreply.github.com>
2022-05-19 01:34:29 +0000

    chore: update api models

---
+++ src/api-clients/catalog-items-api-client-v20220401.ts
@@ -0,0 +1,14 @@
+import { CatalogApi, Configuration } from '../api-models/catalog-items-api-model-v20220401'
+import { ApiClientHelpers } from '../helpers'
+import { DEFAULT_API_BASE_PATH } from '../types'
+import { APIConfigurationParameters } from '../types/api-clients/api-configuration-parameters'
+
+export class CatalogItemsApiClientV20220401 extends CatalogApi {
+  constructor(parameters: APIConfigurationParameters) {
+    const axios = ApiClientHelpers.getAxiosInstance(parameters)
+
+    const configuration = new Configuration(parameters)
+
+    super(configuration, DEFAULT_API_BASE_PATH, axios)
+  }
+}
