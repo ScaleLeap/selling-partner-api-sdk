26f19df51f008633414bf7ee9a7007d059116a6c
nguyentoanit <nguyentoanit@users.noreply.github.com>
2022-08-31 01:34:55 +0000

    chore: update api models

--- src/api-models/fulfillment-inbound-api-model/api.ts
+++ src/api-models/fulfillment-inbound-api-model/api.ts
@@ -1599,7 +1599,7 @@ export interface PartneredSmallParcelDataInput {
      */
     PackageList?: Array<PartneredSmallParcelPackageInput>;
     /**
-     * The Amazon-partnered carrier to use for the inbound shipment.
+     * The Amazon-partnered carrier to use for the inbound shipment. **`CarrierName`** values in France (FR), Italy (IT), Spain (ES), the United Kingdom (UK), and the United States (US): `UNITED_PARCEL_SERVICE_INC`. <br> **`CarrierName`** values in Germany (DE): `DHL_STANDARD`,`UNITED_PARCEL_SERVICE_INC`. <br>Default: `UNITED_PARCEL_SERVICE_INC`.
      * @type {string}
      * @memberof PartneredSmallParcelDataInput
      */
