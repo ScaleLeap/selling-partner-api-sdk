1a56d8eb5b1d3ec45a5d8148d310826394b2cd84
nguyentoanit <19872073+nguyentoanit@users.noreply.github.com>
2024-09-26 01:32:21 +0000

    chore: update api models

---
+++ src/api-clients/invoices-api-client.ts
@@ -0,0 +1,14 @@
+import { Configuration, InvoicesApi } from '../api-models/invoices-api-model'
+import { ApiClientHelpers } from '../helpers'
+import { DEFAULT_API_BASE_PATH } from '../types'
+import { APIConfigurationParameters } from '../types/api-clients/api-configuration-parameters'
+
+export class InvoicesApiClient extends InvoicesApi {
+  constructor(parameters: APIConfigurationParameters) {
+    const axios = ApiClientHelpers.getAxiosInstance(parameters)
+
+    const configuration = new Configuration(parameters)
+
+    super(configuration, DEFAULT_API_BASE_PATH, axios)
+  }
+}
